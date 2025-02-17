1a14dd053d1cd51de669d789f4f65c703a41f26d
rujang <kimbz123@gmail.com>
2025-02-17 13:52:37 +0900

    first commit

---
+++ src/main/java/rujang/OJT_board/OjtBoardApplication.java
@@ -0,0 +1,13 @@
+package rujang.OJT_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjtBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OjtBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rujang/OJT_board/OjtBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package rujang.OJT_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OjtBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
